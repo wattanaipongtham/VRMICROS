4eccd49db84ba0b466836fa83d5bf5ed9fde9334
Wattanai Pongtham <wattanaipongtham@gmail.com>
2024-07-21 16:48:20 +0700

    Serial

--- hardware/vrmicros/0.0.1/cores/arduino/vrmicros/serial.cpp
+++ hardware/vrmicros/0.0.1/cores/arduino/vrmicros/serial.cpp
@@ -3,27 +3,36 @@
 #include <string>
 #include "usbd_cdc_if.h"
 
-#define crnl "\r\n"
+const char* crnl = "asdf";
+uint8_t* println_buf[128];
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
 extern "C"{
 	void delay(uint32_t);
 }
 
 int i=0;
+uint8_t print_state = 0;
 
 void Serial::print(const char* msg){
-	uint8_t* print_buf[128];
-	uint8_t len = strlen(msg);
-
-	memcpy(print_buf, msg, strlen(msg));
-
-	CDC_Transmit_FS((uint8_t*)print_buf, len);
-}
-void Serial::println(const char* msg){
-	uint8_t* println_buf[128];
+	//uint8_t* print_buf[128];
 	uint8_t len = strlen(msg);
 
 	memcpy(println_buf, msg, strlen(msg));
-	memcpy(println_buf+len, "\r\n", 2);
+
+	CDC_Transmit_FS((uint8_t*)println_buf, len);
+}
+void Serial::println(const char* msg){
+	//uint8_t* println_buf[128];
+	uint8_t len = strlen(msg);
+
+	USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+
+  if (hcdc->TxState == 0){
+    memcpy(println_buf, msg, len);
+	memcpy(println_buf+len, crnl, 4);
+	CDC_Transmit_FS((uint8_t*)println_buf, len+6);
+  }
 	
-	CDC_Transmit_FS((uint8_t*)println_buf, len+2);
 }
